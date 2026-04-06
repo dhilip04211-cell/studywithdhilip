fa27547400f8e46c565c7adaf8b72a2506f2462d
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-06 22:18:21 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Study With Dhilip</h1>
+      <p>Your React application is now running!</p>
+      
+      {/* You can add more components or HTML here */}
+      <button onClick={() => alert('Happy Learning!')}>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default App
