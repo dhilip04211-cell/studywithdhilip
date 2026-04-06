66b23a4f580259d9e84ea56278cc90c132b4bb19
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-06 17:52:42 +0000

    Final build and deployment setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import PartFourFive from './PartFourFive'; 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Study With Dhilip</h1>
-      <p>Your React application is now running!</p>
-      
-      {/* You can add more components or HTML here */}
-      <button onClick={() => alert('Happy Learning!')}>
-        Click Me
-      </button>
-    </div>
-  )
+    // basename ensures the links work on GitHub's sub-folder
+    <BrowserRouter basename="/studywithdhilip">
+      <Routes>
+        <Route path="/" element={
+          <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Assistant Engineer (EEE)</h1>
+            {/* Link stays inside the React App */}
+            <Link to="/part4" className="btn">Part-4 and 5</Link>
+            
+            {/* If you still want to link to your old HTML files in the public folder: */}
+            <br /><br />
+            <a href="./Tamil/Tamil.html" className="btn">Back to Tamil (HTML)</a>
+          </div>
+        } />
+        
+        <Route path="/part4" element={<PartFourFive />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+// THIS MAKES main.jsx HAPPY:
+export default App;
