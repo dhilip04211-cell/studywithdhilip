37b7614cbbccad4fedb909b971fa8216012dc5de
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-06 21:29:11 +0000

    Fixed direct part4 routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
-import PPPartOne from "./PPPpart-1";
+import { HashRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import PPPartOne from "./PPPoart-1";
 import PartFourFive from "./PartFourFive";
 
 function Home() {
@@ -8,18 +8,9 @@ function Home() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Assistant Engineer (EEE)</h1>
 
-      <div style={{ display: "flex", flexDirection: "column", gap: "15px", alignItems: "center" }}>
-        <Link to="/part1" className="btn">
-          Part-1
-        </Link>
-
-        <Link to="/part4" className="btn">
-          Part-4 and 5
-        </Link>
-
-        <a href="./Tamil/Tamil.html" className="btn">
-          Back to Tamil (HTML)
-        </a>
+      <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+        <Link to="/part1" className="btn">Part-1</Link>
+        <Link to="/part4" className="btn">Part-4 and 5</Link>
       </div>
     </div>
   );
@@ -32,6 +23,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/part1" element={<PPPartOne />} />
         <Route path="/part4" element={<PartFourFive />} />
+
+        {/* fallback route */}
+        <Route path="*" element={<Navigate to="/part4" replace />} />
       </Routes>
     </HashRouter>
   );
