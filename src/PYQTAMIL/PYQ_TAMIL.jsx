7da051200a10eb23c5d7c2a07bcc0368bde77a55
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-07 21:26:02 +0530

    Update PYQ_TAMIL.jsx

--- src/PYQTAMIL/PYQ_TAMIL.jsx
+++ src/PYQTAMIL/PYQ_TAMIL.jsx
@@ -6,7 +6,7 @@ export default function PYQ_TAMIL() {
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "30px" }}>
       <h1>Previous Year 2025 Tamil</h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "15px", marginTop: "20px" }}>
-        <Link to="/pyq-tamil/1.special-examination-2026" className="btn">1. Special Exam - 2025</Link>
+        <Link to="/PYQ-TAMIL/1.special-examination-2026" className="btn">1. Special Exam - 2025</Link>
         <Link to="/current-affairs/feb-2026" className="btn">February 2026</Link>
         <Link to="/current-affairs/mar-2026" className="btn">March 2026</Link>
         <Link to="/current-affairs/apr-2026" className="btn">April 2026</Link>
