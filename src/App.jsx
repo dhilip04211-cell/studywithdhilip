279d6f78da58c69069e27edf617e7045f06f13e1
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-07 11:01:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { HashRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import PPPartOne from "./PPPpart-1";
 import PartFourFive from "./PartFourFive";
 
+// Dictionary imports
+import Dictionary from "./Dictionary/Dictionary";
+import A_Series from "./Dictionary/A_Series";
+import A_Part_1 from "./Dictionary/A_Series/A_Part_1";
+// Add more parts as you create them:
+// import A_Part_2 from "./Dictionary/A_Part_2";
+// import A_Part_3 from "./Dictionary/A_Part_3";
+// import A_Part_4 from "./Dictionary/A_Part_4";
+// import A_Part_5 from "./Dictionary/A_Part_5";
+
 function Home() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh", width: "100%" }}>
@@ -26,13 +36,26 @@ export default function App() {
   return (
     <HashRouter>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
+
+        {/* AE EEE routes */}
         <Route path="/part1" element={<PPPartOne />} />
         <Route path="/parts" element={<PartFourFive />} />
         <Route path="/part4" element={<PartFourFive defaultPart={4} />} />
         <Route path="/part5" element={<PartFourFive defaultPart={5} />} />
 
-        {/* fallback route */}
+        {/* Dictionary routes */}
+        <Route path="/dictionary" element={<Dictionary />} />
+        <Route path="/dictionary/a-series" element={<A_Series />} />
+        <Route path="/dictionary/a-series/a-part1" element={<A_Part_1 />} />
+        {/* Add more as you build them: */}
+        {/* <Route path="/dictionary/a-series/part2" element={<A_Part_2 />} /> */}
+        {/* <Route path="/dictionary/a-series/part3" element={<A_Part_3 />} /> */}
+        {/* <Route path="/dictionary/a-series/part4" element={<A_Part_4 />} /> */}
+        {/* <Route path="/dictionary/a-series/part5" element={<A_Part_5 />} /> */}
+
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
