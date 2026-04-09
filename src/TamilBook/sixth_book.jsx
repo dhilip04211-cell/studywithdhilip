869cd8b0cc2169ce34c0058102553ee5c65d9b02
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-09 23:27:11 +0530

    Create sixth_book.jsx

---
+++ src/TamilBook/sixth_book.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function sixt_book() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "30px" }}>
+      <h1>Thirukkural</h1>
+      <div style={{ display: "flex", flexDirection: "column", gap: "15px", marginTop: "20px" }}>
+        {/* ✅ Path matches App.jsx route exactly */}
+        <Link to="/TamilBook/ilakkanam-new" className="btn">
+          1. Ilakkanam
+        </Link>
+        {/* Add more exam links here as you create the components */}
+      </div>
+    </div>
+  );
+}
