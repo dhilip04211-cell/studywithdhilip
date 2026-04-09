84d2c049cf714d114a6ea47fd8860a770ef14d71
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-10 00:16:02 +0530

    Update ilakkanam_new.jsx

--- src/TamilBook/ilakkanam_new.jsx
+++ src/TamilBook/ilakkanam_new.jsx
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 export default function ilakkanam_new() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "30px" }}>
-      <h1>Thirukkural</h1>
+      <h1>இலக்கணம்</h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "15px", marginTop: "20px" }}>
-        
+
         {/* JSX Link - Term 1 */}
         <Link to="/Tamilbook/term1-grammar" className="btn">
           1. term 1
         </Link>
-         <a href="Tamil/lakkanam/term1_grammar.html" className="btn">
-          2. term 1(HTML)
+
+        {/* HTML files - from public/Tamil/lakkanam/ */}
+        <a href="/Tamil/lakkanam/term1_grammar.html" className="btn">
+          2. term 1 (HTML)
         </a>
-        {/* HTML anchor - Term 2 only */}
-        <a href="Tamil/lakkanam/term2_grammar.html" className="btn">
-          2. term 2
+
+        <a href="/Tamil/lakkanam/term2_grammar.html" className="btn">
+          3. term 2
         </a>
 
         {/* JSX Link - Term 3 */}
         <Link to="/Tamilbook/term3-grammar" className="btn">
-          3. term 3
+          4. term 3
         </Link>
 
       </div>
