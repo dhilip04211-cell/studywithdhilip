832d40b66dadb3288068a098dbcf4ba99cf44790
dhilip04211-cell <dhilip04211@gmail.com>
2026-04-09 23:26:02 +0530

    Create t_newbook.jsx

---
+++ src/TamilBook/t_newbook.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function t_newbook() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "30px" }}>
+      <h1>Thirukkural</h1>
+      <div style={{ display: "flex", flexDirection: "column", gap: "15px", marginTop: "20px" }}>
+        {/* ✅ Path matches App.jsx route exactly */}
+        <Link to="/TamilBook/sixth-book" className="btn">
+          1. 6th Book
+        </Link>
+        {/* Add more exam links here as you create the components */}
+      </div>
+    </div>
+  );
+}
